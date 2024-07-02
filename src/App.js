035e395b0cea56ddd45c4f4c7cc8e76f23a1cc0e
Jarrod Folino <JarrodFolinoFC>
2024-07-02 21:53:15 +0100

    D

--- src/App.js
+++ src/App.js
@@ -1,23 +1,160 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Badge from "react-bootstrap/Badge";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { styles } from "./CodeStyles";
+
+import NavigationBar from "./components/NavigationBar";
+
+import { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState(null);
+  const [index, setIndex] = useState(0);
+
+  const [code, setCode] = useState(null);
+  const [tags, setTags] = useState([]);
+  const [choices, setChoices] = useState([]);
+  const [answer, setAnswer] = useState(null);
+  const [codeStyle, setCodeStyle] = useState(styles["pojoaque"]);
+
+
+
+
+  useEffect(() => {
+    fetch("/data.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        setCode(data[index].function_src);
+        setTags(data[index].tags);
+        setChoices(data[index].choices);
+        setAnswer(data[index].correct_answer);
+      });
+  }, []);
+
+  function handleClick(event) {
+    const choice = event.target.value;
+    if (choice === answer) {
+      alert("Correct!");
+    } else {
+      alert("Incorrect!");
+    }
+  }
+
+  function handleNext() {
+    setIndex(index + 1);
+    setCode(data[index].function_src);
+    setTags(data[index].tags);
+    setChoices(data[index].choices);
+    setAnswer(data[index].correct_answer);
+  }
+
+  function handlePrevious() {
+    setIndex(index - 1);
+    setCode(data[index].function_src);
+    setTags(data[index].tags);
+    setChoices(data[index].choices);
+    setAnswer(data[index].correct_answer);
+  }
+
+  function tooHard() {
+    handleNext();
+  }
+  function helpMe() {}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container style={{ maxWidth: "460px" }}>
+        <NavigationBar>
+          <Nav className="justify-content-end flex-grow-1 pe-3">
+            <NavDropdown
+              title="Code Style"
+              id={`offcanvasNavbarDropdown-expand-xl`}
+            >
+              {
+                Object.keys(styles).map(k => {
+                  return <NavDropdown.Item key={k} onClick={() => setCodeStyle(styles[k])}>
+                    {k}
+                  </NavDropdown.Item>
+                })
+              }
+            </NavDropdown>
+            <ButtonGroup aria-label="Basic example">
+              <Button variant="outline-secondary" size="sm" onClick={handlePrevious}>
+                Previous
+              </Button>
+              <Button variant="outline-secondary" size="sm" onClick={handleNext}>
+                Next
+              </Button>
+            </ButtonGroup>
+          </Nav>
+        </NavigationBar>
+
+        <Row>
+          <Col>What does the last print statement output?</Col>
+          <Col>
+            {tags.map((tag) => (
+              <Badge key={tag} bg="primary">{tag}</Badge>
+            ))}
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <SyntaxHighlighter language="python" wrapLines={true} style={codeStyle}>
+              {code}
+            </SyntaxHighlighter>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <div className="d-grid gap-2">
+              <Button variant="secondary" size="lg" onClick={helpMe}>
+                Assist Me
+              </Button>
+              {choices.map((choice) => (
+                <Button
+                  key={choice}
+                  value={choice}
+                  variant="primary"
+                  size="lg"
+                  onClick={handleClick}
+                >
+                  {choice}
+                </Button>
+              ))}
+              <Button variant="danger" size="lg" onClick={tooHard}>
+                Too Hard
+              </Button>
+            </div>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+          
+
+            
+          </Col>
+          <Col>
+            
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
---
+++ src/CodeStyles.js
@@ -0,0 +1,198 @@
+import a11yDark from "react-syntax-highlighter/dist/esm/styles/hljs/a11y-dark";
+import a11yLight from "react-syntax-highlighter/dist/esm/styles/hljs/a11y-light";
+import agate from "react-syntax-highlighter/dist/esm/styles/hljs/agate";
+import anOldHope from "react-syntax-highlighter/dist/esm/styles/hljs/an-old-hope";
+import androidstudio from "react-syntax-highlighter/dist/esm/styles/hljs/androidstudio";
+import arduinoLight from "react-syntax-highlighter/dist/esm/styles/hljs/arduino-light";
+import arta from "react-syntax-highlighter/dist/esm/styles/hljs/arta";
+import ascetic from "react-syntax-highlighter/dist/esm/styles/hljs/ascetic";
+import atelierCaveDark from "react-syntax-highlighter/dist/esm/styles/hljs/atelier-cave-dark";
+import atelierCaveLight from "react-syntax-highlighter/dist/esm/styles/hljs/atelier-cave-light";
+import atelierDuneDark from "react-syntax-highlighter/dist/esm/styles/hljs/atelier-dune-dark";
+import atelierDuneLight from "react-syntax-highlighter/dist/esm/styles/hljs/atelier-dune-light";
+import atelierEstuaryDark from "react-syntax-highlighter/dist/esm/styles/hljs/atelier-estuary-dark";
+import atelierEstuaryLight from "react-syntax-highlighter/dist/esm/styles/hljs/atelier-estuary-light";
+import atelierForestDark from "react-syntax-highlighter/dist/esm/styles/hljs/atelier-forest-dark";
+import atelierForestLight from "react-syntax-highlighter/dist/esm/styles/hljs/atelier-forest-light";
+import atelierHeathDark from "react-syntax-highlighter/dist/esm/styles/hljs/atelier-heath-dark";
+import atelierHeathLight from "react-syntax-highlighter/dist/esm/styles/hljs/atelier-heath-light";
+import atelierLakesideDark from "react-syntax-highlighter/dist/esm/styles/hljs/atelier-lakeside-dark";
+import atelierLakesideLight from "react-syntax-highlighter/dist/esm/styles/hljs/atelier-lakeside-light";
+import atelierPlateauDark from "react-syntax-highlighter/dist/esm/styles/hljs/atelier-plateau-dark";
+import atelierPlateauLight from "react-syntax-highlighter/dist/esm/styles/hljs/atelier-plateau-light";
+import atelierSavannaDark from "react-syntax-highlighter/dist/esm/styles/hljs/atelier-savanna-dark";
+import atelierSavannaLight from "react-syntax-highlighter/dist/esm/styles/hljs/atelier-savanna-light";
+import atelierSeasideDark from "react-syntax-highlighter/dist/esm/styles/hljs/atelier-seaside-dark";
+import atelierSeasideLight from "react-syntax-highlighter/dist/esm/styles/hljs/atelier-seaside-light";
+import atelierSulphurpoolDark from "react-syntax-highlighter/dist/esm/styles/hljs/atelier-sulphurpool-dark";
+import atelierSulphurpoolLight from "react-syntax-highlighter/dist/esm/styles/hljs/atelier-sulphurpool-light";
+import atomOneDarkReasonable from "react-syntax-highlighter/dist/esm/styles/hljs/atom-one-dark-reasonable";
+import atomOneDark from "react-syntax-highlighter/dist/esm/styles/hljs/atom-one-dark";
+import atomOneLight from "react-syntax-highlighter/dist/esm/styles/hljs/atom-one-light";
+import brownPaper from "react-syntax-highlighter/dist/esm/styles/hljs/brown-paper";
+import codepenEmbed from "react-syntax-highlighter/dist/esm/styles/hljs/codepen-embed";
+import colorBrewer from "react-syntax-highlighter/dist/esm/styles/hljs/color-brewer";
+import darcula from "react-syntax-highlighter/dist/esm/styles/hljs/darcula";
+import dark from "react-syntax-highlighter/dist/esm/styles/hljs/dark";
+import defaultStyle from "react-syntax-highlighter/dist/esm/styles/hljs/default-style";
+import docco from "react-syntax-highlighter/dist/esm/styles/hljs/docco";
+import dracula from "react-syntax-highlighter/dist/esm/styles/hljs/dracula";
+import far from "react-syntax-highlighter/dist/esm/styles/hljs/far";
+import foundation from "react-syntax-highlighter/dist/esm/styles/hljs/foundation";
+import githubGist from "react-syntax-highlighter/dist/esm/styles/hljs/github-gist";
+import github from "react-syntax-highlighter/dist/esm/styles/hljs/github";
+import gml from "react-syntax-highlighter/dist/esm/styles/hljs/gml";
+import googlecode from "react-syntax-highlighter/dist/esm/styles/hljs/googlecode";
+import gradientDark from "react-syntax-highlighter/dist/esm/styles/hljs/gradient-dark";
+import grayscale from "react-syntax-highlighter/dist/esm/styles/hljs/grayscale";
+import gruvboxDark from "react-syntax-highlighter/dist/esm/styles/hljs/gruvbox-dark";
+import gruvboxLight from "react-syntax-highlighter/dist/esm/styles/hljs/gruvbox-light";
+import hopscotch from "react-syntax-highlighter/dist/esm/styles/hljs/hopscotch";
+import hybrid from "react-syntax-highlighter/dist/esm/styles/hljs/hybrid";
+import idea from "react-syntax-highlighter/dist/esm/styles/hljs/idea";
+import irBlack from "react-syntax-highlighter/dist/esm/styles/hljs/ir-black";
+import isblEditorDark from "react-syntax-highlighter/dist/esm/styles/hljs/isbl-editor-dark";
+import isblEditorLight from "react-syntax-highlighter/dist/esm/styles/hljs/isbl-editor-light";
+import kimbieDark from "react-syntax-highlighter/dist/esm/styles/hljs/kimbie.dark";
+import kimbieLight from "react-syntax-highlighter/dist/esm/styles/hljs/kimbie.light";
+import lightfair from "react-syntax-highlighter/dist/esm/styles/hljs/lightfair";
+import lioshi from "react-syntax-highlighter/dist/esm/styles/hljs/lioshi";
+import magula from "react-syntax-highlighter/dist/esm/styles/hljs/magula";
+import monoBlue from "react-syntax-highlighter/dist/esm/styles/hljs/mono-blue";
+import monokaiSublime from "react-syntax-highlighter/dist/esm/styles/hljs/monokai-sublime";
+import monokai from "react-syntax-highlighter/dist/esm/styles/hljs/monokai";
+import nightOwl from "react-syntax-highlighter/dist/esm/styles/hljs/night-owl";
+import nnfxDark from "react-syntax-highlighter/dist/esm/styles/hljs/nnfx-dark";
+import nnfx from "react-syntax-highlighter/dist/esm/styles/hljs/nnfx";
+import nord from "react-syntax-highlighter/dist/esm/styles/hljs/nord";
+import obsidian from "react-syntax-highlighter/dist/esm/styles/hljs/obsidian";
+import ocean from "react-syntax-highlighter/dist/esm/styles/hljs/ocean";
+import paraisoDark from "react-syntax-highlighter/dist/esm/styles/hljs/paraiso-dark";
+import paraisoLight from "react-syntax-highlighter/dist/esm/styles/hljs/paraiso-light";
+import pojoaque from "react-syntax-highlighter/dist/esm/styles/hljs/pojoaque";
+import purebasic from "react-syntax-highlighter/dist/esm/styles/hljs/purebasic";
+import qtcreatorDark from "react-syntax-highlighter/dist/esm/styles/hljs/qtcreator_dark";
+import qtcreatorLight from "react-syntax-highlighter/dist/esm/styles/hljs/qtcreator_light";
+import railscasts from "react-syntax-highlighter/dist/esm/styles/hljs/railscasts";
+import rainbow from "react-syntax-highlighter/dist/esm/styles/hljs/rainbow";
+import routeros from "react-syntax-highlighter/dist/esm/styles/hljs/routeros";
+import schoolBook from "react-syntax-highlighter/dist/esm/styles/hljs/school-book";
+import shadesOfPurple from "react-syntax-highlighter/dist/esm/styles/hljs/shades-of-purple";
+import solarizedDark from "react-syntax-highlighter/dist/esm/styles/hljs/solarized-dark";
+import solarizedLight from "react-syntax-highlighter/dist/esm/styles/hljs/solarized-light";
+import srcery from "react-syntax-highlighter/dist/esm/styles/hljs/srcery";
+import stackoverflowDark from "react-syntax-highlighter/dist/esm/styles/hljs/stackoverflow-dark";
+import stackoverflowLight from "react-syntax-highlighter/dist/esm/styles/hljs/stackoverflow-light";
+import sunburst from "react-syntax-highlighter/dist/esm/styles/hljs/sunburst";
+import tomorrowNightBlue from "react-syntax-highlighter/dist/esm/styles/hljs/tomorrow-night-blue";
+import tomorrowNightBright from "react-syntax-highlighter/dist/esm/styles/hljs/tomorrow-night-bright";
+import tomorrowNightEighties from "react-syntax-highlighter/dist/esm/styles/hljs/tomorrow-night-eighties";
+import tomorrowNight from "react-syntax-highlighter/dist/esm/styles/hljs/tomorrow-night";
+import tomorrow from "react-syntax-highlighter/dist/esm/styles/hljs/tomorrow";
+import vs from "react-syntax-highlighter/dist/esm/styles/hljs/vs";
+import vs2015 from "react-syntax-highlighter/dist/esm/styles/hljs/vs2015";
+import xcode from "react-syntax-highlighter/dist/esm/styles/hljs/xcode";
+import xt256 from "react-syntax-highlighter/dist/esm/styles/hljs/xt256";
+import zenburn from "react-syntax-highlighter/dist/esm/styles/hljs/zenburn";
+
+const styles = {
+    "a11yDark" : a11yDark,
+"a11yLight": a11yLight,
+"agate": agate,
+"anOldHope": anOldHope,
+"androidstudio": androidstudio,
+"arduinoLight": arduinoLight,
+"arta": arta,
+"ascetic": ascetic,
+"atelierCaveDark": atelierCaveDark,
+"atelierCaveLight": atelierCaveLight,
+"atelierDuneDark": atelierDuneDark,
+"atelierDuneLight": atelierDuneLight,
+"atelierEstuaryDark": atelierEstuaryDark,
+"atelierEstuaryLight": atelierEstuaryLight,
+"atelierForestDark": atelierForestDark,
+"atelierForestLight": atelierForestLight,
+"atelierHeathDark": atelierHeathDark,
+"atelierHeathLight": atelierHeathLight,
+"atelierLakesideDark": atelierLakesideDark,
+"atelierLakesideLight": atelierLakesideLight,
+"atelierPlateauDark": atelierPlateauDark,
+"atelierPlateauLight": atelierPlateauLight,
+"atelierSavannaDark": atelierSavannaDark,
+"atelierSavannaLight": atelierSavannaLight,
+"atelierSeasideDark": atelierSeasideDark,
+"atelierSeasideLight": atelierSeasideLight,
+"atelierSulphurpoolDark": atelierSulphurpoolDark,
+"atelierSulphurpoolLight": atelierSulphurpoolLight,
+"atomOneDarkReasonable": atomOneDarkReasonable,
+"atomOneDark": atomOneDark,
+"atomOneLight": atomOneLight,
+"brownPaper": brownPaper,
+"codepenEmbed": codepenEmbed,
+"colorBrewer": colorBrewer,
+"darcula": darcula,
+"dark": dark,
+"defaultStyle": defaultStyle,
+"docco": docco,
+"dracula": dracula,
+"far": far,
+"foundation": foundation,
+"githubGist": githubGist,
+"github": github,
+"gml": gml,
+"googlecode": googlecode,
+"gradientDark": gradientDark,
+"grayscale": grayscale,
+"gruvboxDark": gruvboxDark,
+"gruvboxLight": gruvboxLight,
+"hopscotch": hopscotch,
+"hybrid": hybrid,
+"idea": idea,
+"irBlack": irBlack,
+"isblEditorDark": isblEditorDark,
+"isblEditorLight": isblEditorLight,
+"kimbieDark": kimbieDark,
+"kimbieLight": kimbieLight,
+"lightfair": lightfair,
+"lioshi": lioshi,
+"magula": magula,
+"monoBlue": monoBlue,
+"monokaiSublime": monokaiSublime,
+"monokai": monokai,
+"nightOwl": nightOwl,
+"nnfxDark": nnfxDark,
+"nnfx": nnfx,
+"nord": nord,
+"obsidian": obsidian,
+"ocean": ocean,
+"paraisoDark": paraisoDark,
+"paraisoLight": paraisoLight,
+"pojoaque": pojoaque,
+"purebasic": purebasic,
+"qtcreatorDark": qtcreatorDark,
+"qtcreatorLight": qtcreatorLight,
+"railscasts": railscasts,
+"rainbow": rainbow,
+"routeros": routeros,
+"schoolBook": schoolBook,
+"shadesOfPurple": shadesOfPurple,
+"solarizedDark": solarizedDark,
+"solarizedLight": solarizedLight,
+"srcery": srcery,
+"stackoverflowDark": stackoverflowDark,
+"stackoverflowLight": stackoverflowLight,
+"sunburst": sunburst,
+"tomorrowNightBlue": tomorrowNightBlue,
+"tomorrowNightBright": tomorrowNightBright,
+"tomorrowNightEighties": tomorrowNightEighties,
+"tomorrowNight": tomorrowNight,
+"tomorrow": tomorrow,
+"vs": vs,
+"vs2015": vs2015,
+"xcode": xcode,
+"xt256": xt256,
+"zenburn": zenburn,
+"pojoaque": pojoaque
+}
+
+export { styles };
